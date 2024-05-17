a9f9dff9b3bb8d535e1bca6571a1c66d28034c23
LeeQ19 <leekw1994@gmail.com>
2024-05-18 03:50:40 +0900

    Add prefetch

--- components/billion.tsx
+++ components/billion.tsx
@@ -12,7 +12,7 @@ interface IBillionProps {
 export default function Billion({ id, name, square_image, netWorth, industry }: IBillionProps) {
   return (
     <div className={styles.billion}>
-      <Link href={`/person/${id}`}>
+      <Link prefetch href={`/person/${id}`}>
         <img src={square_image} alt={name} />
         <h3>{name}</h3>
         <h4>{Math.round(netWorth * 1e-3)} Billion  |  {industry}</h4>
